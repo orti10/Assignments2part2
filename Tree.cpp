ff128a6a88dbbbafa3d351b8498c8c6276ad7177
orti <orti@gmail.com>
2019-03-25 20:06:14 +0200

    first commit

---
+++ Tree.cpp
@@ -0,0 +1,202 @@
+/**
+ * exercise on binary trees
+ *
+ * @author Ortal and Tomer
+ *
+ * http://www.cplusplus.com/forum/beginner/118562/
+ */
+#include <iostream>
+#include "Tree.hpp"
+using namespace ariel;
+using namespace std;
+
+Tree::Tree() {
+    treeRoot=new Node() ;
+    sizeOf=0;
+}
+Tree::~Tree() {
+    deleteTree(treeRoot);
+}
+void Tree::deleteTree(Node *n){
+    if(n){
+        deleteTree(n->getLeft());
+        deleteTree(n->getRight());
+        delete n;
+    }
+}
+
+void Tree::insert(int i) {
+	if(treeRoot==nullptr){
+	    treeRoot=new Node(i);
+	}
+	else{
+	    Node *n = treeRoot;
+	    while(n!=nullptr){
+	        if(i>n->getKey()){
+	            n=n->getRight();
+	        }
+	    else if(i<n->getKey()){
+	        n=n->getLeft();
+	    }
+	    else{
+	        throw runtime_error("the value already exist");
+	    }
+	  }
+	  n=new Node(i);  
+	}
+	++sizeOf;
+}
+
+Node Tree::minNodeValue(Node *n){
+    Node *temp=n;
+    
+    while(temp->getLeft()!=nullptr){
+         minNodeValue(temp->getLeft());
+    }
+    return temp;
+}
+
+void Tree::remove(int i) {
+	if(!contains()){
+	     throw runtime_error("there is no such key");
+	}
+	else{
+	    Node *n=treeRoot;
+	    while(n!=nullptr){
+	        if(i>n->getKey()){
+	            n=n->getRight();
+	        }
+	        else if(i<n->getKey()){
+	            n=n->getLeft();
+	        }
+	        else{
+	            if(n->getLeft()==nullptr){
+	                Node *temp=n->getRight();
+	                
+	            }
+	        }
+	    }
+	        
+	}
+	    
+	}
+}
+
+int Tree::size() {
+	return sizeOf;
+}
+
+bool Tree::contains(int i) {
+    if(treeRoot==nullptr){
+        return false;
+    }
+    Node *n=treeRoot;
+    while (n!=nullptr){
+      if(i>n->getKey()){
+	            n=n->getRight();
+	        }
+	    else if(i<n->getKey()){
+	        n=n->getLeft();
+	    }
+	    else{
+	        return true;
+	    }  
+    }
+    return false;
+}
+
+int Tree::root() {
+    if(treeRoot!=nullptr){
+    return treeRoot->getKey();
+    }
+    throw runtime_error("the tree is empty");
+}
+
+int Tree::parent(int i) {
+	if((!contains(i)) && (i==treeRoot->getKey())){
+	    throw runtime_error("there no such key");
+	}
+	
+	else{
+	    Node *n=treeRoot;
+	    while(1){
+	    if(i>n->getKey()){
+	        if(n->getRight()->getKey()==i){
+	            return n->getKey();
+	        }
+	        else n=n->getRight();
+	    }
+	    else {
+	        if(n->getLeft()->getKey()==i){
+	            return n->getKey();
+	        }
+	        else n=n->getLeft();
+	    }
+	}
+  }
+  return NULL;
+}
+
+
+int Tree::left(int i) {
+	if(!contains(i)){
+	   throw runtime_error("there no such key");
+	}
+	Node *n=treeRoot;
+	else{
+	    while(n!=nullptr){
+	        if(i==n->getKey()){
+	            if(n->getLeft()==nullptr){
+	                throw runtime_error("there no such key");
+	            }
+	            else return n->getLeft()->getKey();
+	        }
+	        else if(i>n->getKey()){
+	            n=n->getRight();
+	        }
+	        else
+	        n=n->getLeft();
+        }
+    }
+    return NULL;
+}
+int Tree::right(int i) {
+	if(!contains(i)){
+	   throw runtime_error("there no such key");
+	}
+	Node *n=treeRoot;
+	else{
+	    while(n!=nullptr){
+	        if(i==n->getKey()){
+	            if(n->getRight()==nullptr){
+	                throw runtime_error("there no such key");
+	            }
+	            else return n->getRight()->getKey();
+	        }
+	        else if(i>n->getKey()){
+	            n=n->getRight();
+	        }
+	        else
+	        n=n->getLeft();
+        }
+    }
+    return NULL;
+}
+//Inorder print LVR
+void Tree::printInOrder(Node *n) {
+    if(n==nullptr) return;
+
+	printInOrder(n->getLeft());
+	cout<<n->getKey()<<" ";
+	printInOrder(n->getRight());
+
+}
+
+
+void Tree::print() {
+   printInOrder(treeRoot);
+	
+}
+// int main{
+//     cout <<"D" << endl;
+// }
---
+++ Tree.hpp
@@ -0,0 +1,64 @@
+/*
+*
+*
+* http://www.cplusplus.com/forum/beginner/118562/
+*/
+#include <iostream>
+namespace ariel
+{
+	using namespace std;
+
+
+	class Node {
+	private:
+		int key;
+		Node *left;
+		Node *right;
+
+	public:
+		Node(int data) {
+			key = data;
+		}
+		int getKey() {
+			return key;
+		}
+		void setKey(int key) {
+			this->key=key;
+		}
+		Node *getRight() {
+			return right;
+		}
+		Node *getLeft() {
+			return left;
+		}
+		void *setRight(Node *node) {
+			this->right = node;
+		}
+		void *setLeft(Node *node) {
+			this->left = node;
+		}
+
+	};
+
+	class Tree {
+	private:
+		Node *treeRoot;
+		int sizeOf;
+
+	public:
+		Tree();
+		~Tree();
+		void insert(int i);
+	    void deleteTree(Node *n);
+		void remove(int i);
+		Node minNodeValue(Node *n);
+		int size();
+		bool contains(int i);
+		int root();
+		int parent(int i);
+		int left(int i);
+		int right(int i);
+	    void printInOrder(Node *n);
+		void print();
+	};
+}
